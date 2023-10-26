48552bccb74e22b6a6c4750f43a73adda2d04d62
Raymart <raymartformalejo00@gmail.com>
2023-10-26 18:08:55 +0800

    Hero center the title and button

--- src/pages/home/components/Hero.tsx
+++ src/pages/home/components/Hero.tsx
@@ -6,12 +6,14 @@ const Hero = () => {
   return (
     <section className={classes["hero-container"]}>
       <img className={classes["background-image"]} src={backgroundImage} />
-      <h1 className={classes.title}>
-        <span>Luxury</span> <span>fashion</span> <span>& Accessories</span>
-      </h1>
-      <Link to="/products" className={classes.button}>
-        Explore collection
-      </Link>
+      <div>
+        <h1 className={classes.title}>
+          <span>Luxury</span> <span>fashion</span> <span>& Accessories</span>
+        </h1>
+        <Link to="/products" className={classes.button}>
+          <button>Explore collection</button>
+        </Link>
+      </div>
     </section>
   );
 };
