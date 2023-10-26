ebdb65069657565d4be6ff8facb4e1bef70c4150
Raymart <raymartformalejo00@gmail.com>
2023-10-26 18:45:55 +0800

    Responsive Hero

--- src/pages/home/components/Hero.tsx
+++ src/pages/home/components/Hero.tsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-import backgroundImage from "../../../assets/hero-image.png";
+import backgroundImage from "../../../assets/hero-img-model.png";
 import classes from "../../../styles/components/home/Hero.module.css";
 
 const Hero = () => {
   return (
-    <section className={classes["hero-container"]}>
+    <section className={` ${classes["hero-container"]}`}>
       <img className={classes["background-image"]} src={backgroundImage} />
-      <div>
+      <div className={`container ${classes["hero-content"]}`}>
         <h1 className={classes.title}>
           <span>Luxury</span> <span>fashion</span> <span>& Accessories</span>
         </h1>
