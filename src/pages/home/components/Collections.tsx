15116a2437772d3053e90499c077b31772e1bb11
Raymart <raymartformalejo00@gmail.com>
2023-11-04 03:11:42 +0800

    Responsive Collection section

--- src/pages/home/components/Collections.tsx
+++ src/pages/home/components/Collections.tsx
@@ -1,56 +1,47 @@
 import CollectionImage1 from "../../../assets/collectionImage1.png";
 import CollectionImage2 from "../../../assets/collectionImage2.png";
-import CollectionImage3 from "../../../assets/CollectionImage3.png";
 import classes from "../../../styles/components/home/Collections.module.css";
 const Collections = () => {
   return (
-    <section className={classes["collection-container"]}>
+    <section className={` ${classes["collection"]}`}>
       <h3 className={`title ${classes["title-collection"]}`}>Collections</h3>
-      <div className={classes["collection-item-wrapper"]}>
-        <h4
-          className={`${classes["collection-item-title"]} ${classes["october-collection-title"]}`}
+      <div className={` ${classes["collection-container"]}`}>
+        <div
+          className={`${classes["collection-item-wrapper"]} ${classes["october-collection"]}`}
         >
-          October
-          <span className={classes["october-collection-title__date"]}>10</span>
-          <span>Collection</span>
-        </h4>
+          <h4
+            className={`${classes["collection-item-title"]} ${classes["october-collection-title"]}`}
+          >
+            October
+            <span className={classes["october-collection-title__date"]}>
+              10
+            </span>
+            <span>Collection</span>
+          </h4>
 
-        <img
-          className={classes["collection-item-bg-image"]}
-          src={CollectionImage1}
-          alt="Collection"
-        />
-      </div>
+          <img
+            className={classes["collection-item-bg-image"]}
+            src={CollectionImage1}
+            alt="Collection"
+          />
+        </div>
 
-      <div
-        className={`${classes["collection-item-wrapper"]} ${classes["autumn-collection-wrapper"]}`}
-      >
-        <h4
-          className={`${classes["collection-item-title"]} ${classes["autumn-collection-title"]}`}
+        <div
+          className={`${classes["collection-item-wrapper"]} ${classes["autumn-collection"]}`}
         >
-          Autumn
-          <span>Collection</span>
-        </h4>
+          <h4
+            className={`${classes["collection-item-title"]} ${classes["autumn-collection-title"]}`}
+          >
+            Autumn
+            <span>Collection</span>
+          </h4>
 
-        <img
-          className={classes["collection-item-bg-image"]}
-          src={CollectionImage2}
-          alt="Collection"
-        />
-      </div>
-
-      <div className={classes["collection-item-wrapper"]}>
-        <h4
-          className={`${classes["collection-item-title"]} ${classes["spring-collection-title"]}`}
-        >
-          Spring
-          <span>Collection</span>
-        </h4>
-        <img
-          className={classes["collection-item-bg-image"]}
-          src={CollectionImage3}
-          alt="Collection"
-        />
+          <img
+            className={classes["collection-item-bg-image"]}
+            src={CollectionImage2}
+            alt="Collection"
+          />
+        </div>
       </div>
     </section>
   );
